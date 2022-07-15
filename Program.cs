502311d18a7c690f7548fb8b1769762c00df7197
Eren Solmaz <solmazeren2009@hotmail.com>
2022-07-15 03:32:55 +0300

    ilk commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace console_programlama
+{
+    class Program 
+    {
+         public static void Main(string[] args)
+         {
+            Console.WriteLine("İsminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            }
+         }
+      }
